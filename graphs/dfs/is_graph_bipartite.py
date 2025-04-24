c564f25747b2b62318442c7bf40081b2b732b8d2
swetha21052001 <91145318+swetha21052001@users.noreply.github.com>
2025-04-24 21:21:22 +0530

    Update is_graph_bipartite.py

--- graphs/dfs/is_graph_bipartite.py
+++ graphs/dfs/is_graph_bipartite.py
@@ -5,16 +5,19 @@ class Solution:
     self.isBipartite = True
     def dfs(node,color):
       if self.visited[node]:
+        #if a node is already visited and already coloured with a different color, the graph is not a bipartite
         if self.colour[node]!=color:
           self.isBipartite = False
         return
       self.visited[node] = True
       self.colour[node] = color
+      #if the color of parent is one, color the neighbor with 2 and vice versa
       for neigh in graph[node]:
         if color == 1:
           dfs(neigh,2)
         else:
           dfs(neigh,1)
+    #parse all unvisited nodes
     for i in range(len(graph)):
       if not self.visited[i]:
         dfs(i, 1)
